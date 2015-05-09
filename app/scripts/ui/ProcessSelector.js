4c86feaf3c597eb2e8449d557a0e2acfa28b9a9b
Ole André Vadla Ravnås <oleavr@gmail.com>
2015-05-09 14:44:14 +0200

    Fix styling of the process selector

--- app/scripts/ui/ProcessSelector.js
+++ app/scripts/ui/ProcessSelector.js
@@ -10,30 +10,26 @@ const ProcessSelector = React.createClass({
       <div className="process-selector jumbotron">
         <h1>Welcome!</h1>
         <h4>Select a process to get started:</h4>
-        <form className="form-horizontal">
-          <div className="control-group">
-            <label className="control-label">Device</label>
-            <div className="controls">
+        <form>
+          <div className="form-group">
+            <label>Device</label>
+            <div>
               <select ref="devices">
                 <option value="1">Local System</option>
                 <option value="2">iPhone</option>
               </select>
             </div>
           </div>
-          <div className="control-group">
-            <label className="control-label">Process</label>
-            <div className="controls">
+          <div className="form-group">
+            <label>Process</label>
+            <div>
               <select ref="processes">
                 <option value="1234">Facebook</option>
                 <option value="5678">Twitter</option>
               </select>
             </div>
           </div>
-          <div className="control-group">
-            <div className="controls">
-              <button className="btn btn-primary large" data-action="attach">Start</button>
-            </div>
-          </div>
+          <button type="submit" className="btn btn-primary large">Start</button>
         </form>
       </div>
     );
